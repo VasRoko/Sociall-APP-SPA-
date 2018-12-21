b74458f85542d35c91c1c9942ad8a4a09b184fd4
Vas <vasrokochuk@gmail.com>
2018-12-21 14:50:17 +0000

    Member Details Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { AppComponent } from './app.component';
 import { HttpClient } from 'selenium-webdriver/http';
@@ -12,7 +13,7 @@ import { NavComponent } from './nav/nav.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyjsService } from './_services/alertifyjs.service';
-import { BsDropdownModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
@@ -20,7 +21,11 @@ import { appRoutes } from 'src/routes';
 import { AuthGuard } from './_guards/auth.guard';
 import { UserService } from './_services/user.service';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
+export function tokenGetter() {
+   return localStorage.getItem('token');
+}
 
 @NgModule({
    declarations: [
@@ -31,14 +36,23 @@ import { MemberCardComponent } from './members/member-card/member-card.component
       MemberListComponent,
       ListsComponent,
       MessagesComponent,
-      MemberCardComponent
+      MemberCardComponent,
+      MemberDetailComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BsDropdownModule.forRoot(),
-      RouterModule.forRoot(appRoutes)
+      TabsModule.forRoot(),
+      RouterModule.forRoot(appRoutes),
+      JwtModule.forRoot({
+         config: {
+            tokenGetter: tokenGetter,
+            whitelistedDomains: ['localhost:5001'],
+            blacklistedRoutes: ['localhost:5001/api/auth']
+         }
+      })
    ],
    providers: [
       AuthService,
--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,11 @@ import { MemberListComponent } from './app/members/member-list/member-list.compo
 import { MessagesComponent } from './app/messages/messages.component';
 import { ListsComponent } from './app/lists/lists.component';
 import { AuthGuard } from './app/_guards/auth.guard';
+import { MemberDetailComponent } from './app/members/member-detail/member-detail.component';
 
 export const appRoutes: Routes = [
     { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
+    { path: 'members/:id', component: MemberDetailComponent, canActivate: [AuthGuard] },
     { path: 'messages', component: MessagesComponent, canActivate: [AuthGuard] },
     { path: 'lists', component: ListsComponent, canActivate: [AuthGuard] },
     // If application has more than 5 protected routers user below
