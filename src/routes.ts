1690a857686ea3d2b4c63c9631dc778deebbc6f9
Vas <vasrokochuk@gmail.com>
2018-12-21 15:42:16 +0000

    Resolvers

--- src/routes.ts
+++ src/routes.ts
@@ -4,10 +4,12 @@ import { MessagesComponent } from './app/messages/messages.component';
 import { ListsComponent } from './app/lists/lists.component';
 import { AuthGuard } from './app/_guards/auth.guard';
 import { MemberDetailComponent } from './app/members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './app/_resolvers/member-detail.resolver';
+import { MemberListResolver } from './app/_resolvers/member-list.resolver';
 
 export const appRoutes: Routes = [
-    { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
-    { path: 'members/:id', component: MemberDetailComponent, canActivate: [AuthGuard] },
+    { path: 'members', component: MemberListComponent, canActivate: [AuthGuard], resolve: {users: MemberListResolver} },
+    { path: 'members/:id', component: MemberDetailComponent, canActivate: [AuthGuard], resolve: {user: MemberDetailResolver} },
     { path: 'messages', component: MessagesComponent, canActivate: [AuthGuard] },
     { path: 'lists', component: ListsComponent, canActivate: [AuthGuard] },
     // If application has more than 5 protected routers user below
